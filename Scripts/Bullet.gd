7d09444117ae295096c70461c0399ebd97d1e152
Cameron Dodd <cameronradodd@gmail.com>
2019-08-04 05:12:01 -0700

    Removed bullet trails (for realz this time)

--- Scripts/Bullet.gd
+++ Scripts/Bullet.gd
@@ -6,11 +6,11 @@ var bulletSpeed = 750;
 var gun
 var gunOwner
 
-var startPos = Vector2()
+#var startPos = Vector2()
 var velocity = Vector2()
 
-var trailColor 
-var trailWidth = 0.3
+#var trailColor 
+#var trailWidth = 0.3
 
 signal BulletHit
 
@@ -21,12 +21,10 @@ func Start(pos, dir, gun):
 	
 	rotation = dir
 	position = pos
-	startPos = pos
+	#startPos = pos
 	
 	velocity = Vector2(bulletSpeed, 0).rotated(rotation)
 	#hitsLeft = pen
-	
-	trailColor = Color(255, 0, 0, 0.01)
 	
 	self.gun = gun
 	gunOwner = gun.gunOwner
@@ -37,7 +35,6 @@ func Start(pos, dir, gun):
 	else:
 		add_to_group("Enemy")
 		print ("setEnemy")
-	
 	
 	#------------masks layer of owner----------
 #	var pl = gunOwner.get_collision_layer()
@@ -54,13 +51,6 @@ func Start(pos, dir, gun):
 		set_collision_mask_bit(2, false)
 		
 	print("bulletcol ", get_collision_mask())
-	
-
-func _draw():
-	draw_line(Vector2(0, 0), startPos-global_position, trailColor, trailWidth, false )
-
-func _process(delta):
-	update()
 
 func _physics_process(delta): #---------------------------------------------
 	
