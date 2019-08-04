d55935ced0c238aff6fc59503d42294c63bbce3b
Evan <maskedmonkyman@gmail.com>
2019-08-04 00:31:15 -0700

    Ai was made

---
+++ Scripts/enemyVars.gd
@@ -0,0 +1,8 @@
+extends Position2D
+
+export var patrolRad = 100
+export var patrolAgroRadius = 200;
+export var deAgroRadius = 300;
+export var chaseDist = 200;
+export var aimSpeed = 0.5;
+export var aimRange = 100;
